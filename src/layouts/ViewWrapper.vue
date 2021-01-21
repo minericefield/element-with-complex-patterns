c85242ecb94f75910e0e1882f66cc4fb05f14161
minericefield <minericefield@gmail.com>
2021-01-22 00:12:10 +0900

    add wrapper for basic layout

---
+++ src/layouts/ViewWrapper.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-container
+    class="view-wraper"
+  >
+    <el-aside
+      width="64px"
+    >
+      <i
+        class="el-icon-notebook-1"
+        @click="showInformation"
+      />
+    </el-aside>
+    <el-main
+      class="view-wraper_main"
+    >
+      <slot />
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  props: {
+    information: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    showInformation () {
+      this.$alert(this.information, 'Note', {
+        confirmButtonText: 'OK',
+        dangerouslyUseHTMLString: true
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.view-wraper {
+  height: 100%;
+  ::v-deep i {
+    font-size: 32px;
+    color: teal;
+    cursor: pointer;
+  }
+  &_main {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+}
+</style>
