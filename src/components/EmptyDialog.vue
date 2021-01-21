961c9d3bbf4c3ec4b8109db62f21984e75de2297
minericefield <minericefield@gmail.com>
2021-01-22 00:30:26 +0900

    add empty dialog

---
+++ src/components/EmptyDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-dialog
+    title="Empty dialog"
+    :visible.sync="isDialogVisible"
+    width="64%"
+    @close="$emit('onClose')">
+    <span>Empty dialog</span>
+    <span slot="footer">
+      <el-button @click="isDialogVisible = false">Close</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isDialogVisible: false
+    }
+  },
+  async mounted () {
+    await this.$nextTick()
+    this.isDialogVisible = true
+  }
+}
+</script>
